33ce877a4713eaf24af4ab04c679f33648f48af5
George Chearswat <chearswat@gmail.com>
2015-08-30 01:06:27 -0700

    Changed main font, and fixed timestamp for weather forecast

--- js/main.js
+++ js/main.js
@@ -341,7 +341,7 @@ jQuery(document).ready(function($) {
 				var dt = new Date(forecast.timestamp);
 				var row = $('<tr />').css('opacity', opacity);
 
-				row.append($('<td/>').addClass('day').html(moment.weekdaysShort(dt.getDay())));
+				row.append($('<td/>').addClass('day').html(moment.weekdaysShort(dt.getUTCDay())));
 				row.append($('<td/>').addClass('icon-small').addClass(iconClass));
 				row.append($('<td/>').addClass('temp-max').html(roundVal(Math.round(forecast.temp_max)) + "&deg;"));
 				row.append($('<td/>').addClass('temp-min').html(roundVal(Math.round(forecast.temp_min)) + "&deg;"));
